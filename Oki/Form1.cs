8203f1d921661ee638adc6b0147ebe457a71b765
NathanDLR <natandelreal@gmail.com>
2021-02-16 10:12:00 +0100

    Load Data

--- Oki/Form1.cs
+++ Oki/Form1.cs
@@ -3,21 +3,41 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace Oki
 {
     public partial class frmOki : Form
     {
+        // Empty xml file
+        XmlDocument xmldoc = new XmlDocument();
+
+        // Files array
+        private String[] files;
+
         public frmOki()
         {
             InitializeComponent();
+            loadData();
         }
 
+        // Load XML's files from C:/xml's
+        public void loadData()
+        {
+            // Load files names into listbox
+            files = Directory.GetFiles(@"C:\xml's");
 
+            foreach(String file in files)
+            {
+                // Add file to list
+                lstArchivos.Items.Add(Path.GetFileName(file));
+            }            
+        }
 
         // Click on btnProcesar
         private void btnProcesar_Click(object sender, EventArgs e)
