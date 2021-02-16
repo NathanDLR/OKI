c097a870e92e321c138d96bb74b10a37a147d8c5
NathanDLR <natandelreal@gmail.com>
2021-02-16 08:55:37 +0100

    Initial commit

---
+++ Oki/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace Oki
+{
+    partial class frmOki
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstArchivos = new System.Windows.Forms.ListBox();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstArchivos
+            // 
+            this.lstArchivos.FormattingEnabled = true;
+            this.lstArchivos.Location = new System.Drawing.Point(55, 32);
+            this.lstArchivos.Name = "lstArchivos";
+            this.lstArchivos.Size = new System.Drawing.Size(482, 433);
+            this.lstArchivos.TabIndex = 0;
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnProcesar.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.btnProcesar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProcesar.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcesar.Location = new System.Drawing.Point(55, 491);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(236, 74);
+            this.btnProcesar.TabIndex = 1;
+            this.btnProcesar.Text = "PROCESAR";
+            this.btnProcesar.UseVisualStyleBackColor = false;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(301, 491);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(236, 74);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "ENVIAR";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // frmOki
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(592, 659);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.lstArchivos);
+            this.Name = "frmOki";
+            this.Text = "OKI SPAIN";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstArchivos;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Oki/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Oki
+{
+    public partial class frmOki : Form
+    {
+        public frmOki()
+        {
+            InitializeComponent();
+        }
+
+
+
+        // Click on btnProcesar
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
